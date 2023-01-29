abe092457fb8ea3c8b04feb0f539e30a9b60753b
Shinichi <miyakawashi@gmail.com>
2023-01-29 16:18:49 -0500

    current weather get data

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { WeatherData } from "./util/Data";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <WeatherData />;
       </header>
     </div>
   );
--- main/src/util/Data.tsx
+++ main/src/util/Data.tsx
@@ -1,4 +1,51 @@
 export const WeatherData = (): JSX.Element => {
+  const apiKey = "08de60513f8aaba79d4697d19640606f";
+
+  const getCurrentWeatherData = async (
+    cityName: string,
+    apiKey: any
+  ): Promise<object> => {
+    const url = new URL("https://api.openweathermap.org/data/2.5/weather");
+    url.searchParams.append("q", cityName);
+    url.searchParams.append("appid", apiKey);
+    url.searchParams.append("units", "imperial");
+    console.log(url);
+
+    try {
+      const currentWeatherData = await fetch(url).then((response: Response) => {
+        if (!response.ok) {
+          switch (response.status) {
+            case 300:
+              console.error({
+                code: response.status,
+                message: response.statusText,
+              });
+              break;
+            case 404:
+              console.error({
+                code: response.status,
+                message: response.statusText,
+              });
+              break;
+            case 500:
+              console.error({
+                code: response.status,
+                message: response.statusText,
+              });
+              break;
+          }
+        }
+        return response.json();
+      });
+      console.log(currentWeatherData);
+      return currentWeatherData;
+    } catch (error) {
+      console.error(error);
+      return { error: error };
+    }
+  };
+
+  const data = getCurrentWeatherData("Columbus", apiKey);
 
   return (
     <div>
